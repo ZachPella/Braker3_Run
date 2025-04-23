810f26a42c4679a1b44156c0f11a6bdfd45d891a
Zach Pella <127986924+ZachPella@users.noreply.github.com>
2025-04-23 12:22:46 -0500

    Update 4_make_volcano_plots.R

--- braker_run/differential_expression/4_make_volcano_plots.R
+++ braker_run/differential_expression/4_make_volcano_plots.R
@@ -1,1 +1,13 @@
+library(ggplot2)
+
+df <- read.csv("sig_DE_Female_vs_L3.csv")
+df$color <- ifelse(df$logFC > 1, "Up in Female",
+             ifelse(df$logFC < -1, "Up in L3", "Not Sig"))
+
+ggplot(df, aes(x=logFC, y=-log10(FDR), color=color)) +
+  geom_point(alpha=0.6) +
+  theme_minimal() +
+  scale_color_manual(values=c("red", "blue", "gray")) +
+  ggtitle("Volcano Plot: Female vs L3") +
+  ggsave("volcano_female_vs_l3.pdf")
 
