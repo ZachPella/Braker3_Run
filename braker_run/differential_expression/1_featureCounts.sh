4a9a95205d53a112b3d0344a8294bfe2d6321e78
Zach Pella <127986924+ZachPella@users.noreply.github.com>
2025-04-23 12:21:02 -0500

    Update 1_featureCounts.sh

--- braker_run/differential_expression/1_featureCounts.sh
+++ braker_run/differential_expression/1_featureCounts.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=featureCounts
+#SBATCH --output=featureCounts.%j.out
+#SBATCH --error=featureCounts.%j.err
+
+module load subread
+
+featureCounts -p -T 8 -t exon -g gene_id \
+  -a braker.gtf \
+  -o gene_counts.txt \
+  Na-2-female*.bam Na-3-male*.bam Na-L3*.bam
 
