baf6db7bbedd00a55b4b7aa6b920a7091f12e4de
Zach Pella <127986924+ZachPella@users.noreply.github.com>
2025-04-23 12:23:00 -0500

    Update 5_top10_GO_pathway_plots.R

--- braker_run/differential_expression/5_top10_GO_pathway_plots.R
+++ braker_run/differential_expression/5_top10_GO_pathway_plots.R
@@ -1,1 +1,13 @@
+library(ggplot2)
+
+# Load example GO term counts
+go_data <- read.csv("topGO_BP_results.csv")
+
+ggplot(go_data, aes(x=reorder(Term, -Significant), y=Significant)) +
+  geom_bar(stat="identity") +
+  coord_flip() +
+  xlab("GO Term") + ylab("Significant Genes") +
+  ggtitle("Top 10 GO BP Terms") +
+  theme_minimal() +
+  ggsave("top10_go_bp_terms.pdf")
 
